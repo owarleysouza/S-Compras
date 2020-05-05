b92477f2443e5d240540b7427d071c75e8984dcd
Warley Soares <wss@cin.ufpe.br>
2020-05-05 15:37:47 -0300

    Implementada tela basica inicial com scaffold e lista de texto que serao os produtos

--- lib/views/telaInicial.dart
+++ lib/views/telaInicial.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+class TelaInicial extends StatefulWidget {
+  @override
+  _TelaInicialState createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas Compras"),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Feijão"),
+              Text("Arroz"),
+              Text("Desinfetante")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
