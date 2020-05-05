8b2fdb35f73b533045806bc807be169f8f56ad01
Warley Soares <wss@cin.ufpe.br>
2020-05-05 17:14:33 -0300

    Implementado lista de produtos, contador para percorrer a mesma e metodo get para verificar se vazou a lista

--- lib/views/telaInicial.dart
+++ lib/views/telaInicial.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minhas_compras/views/categoria.dart';
 
 class TelaInicial extends StatefulWidget {
   @override
@@ -6,23 +7,30 @@ class TelaInicial extends StatefulWidget {
 }
 
 class _TelaInicialState extends State<TelaInicial> {
+  final List<Map<String, Object>> produtos = [
+    {"nome": "Feijão", "quantidade": 2, "categoria": "Grosso"},
+    {"nome": "Arroz", "quantidade": 5, "categoria": "Grosso"},
+    {"nome": "Desinfetante", "quantidade": 1, "categoria": "Limpeza"},
+    {"nome": "Sabão", "quantidade": 8, "categoria": "Limpeza"},
+  ];
+
+  int _produtonalista = 0;
+
+  get temProdutonaLista {
+    if (_produtonalista < (produtos.length)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Minhas Compras"),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text("Feijão"),
-              Text("Arroz"),
-              Text("Desinfetante")
-            ],
-          ),
-        ),
-      ),
+      body: 
     );
   }
 }
