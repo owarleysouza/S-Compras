555d2fa60c07d315ad74d29d2609c86a58d198cc
Warley Soares <wss@cin.ufpe.br>
2020-05-13 17:52:13 -0300

    criacao da classe produto

---
+++ lib/models/produto.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class Produto {
+  String nome;
+  String quantidade;
+  String categoria;
+
+  Produto(
+      {@required this.nome,
+      @required this.quantidade,
+      @required this.categoria});
+}
