dbeb468a49275bc1805d100191232a9faa083e30
Warley Soares <wss@cin.ufpe.br>
2020-05-15 18:15:48 -0300

    Alterado interface para acomodar os elementos com listview e espaçamento entre os elementos

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -12,27 +12,27 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Material(
       //Aqui é usado o Material porque o TextField precisa ter um ancestral como o Material ou Sccafold para funcionar
-      child: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'images/logo.png',
-              width: 100,
-              height: 100,
-            ),
-            TextField(decoration: InputDecoration(labelText: "E-mail")),
-            TextField(decoration: InputDecoration(labelText: "Senha")),
-            RaisedButton(
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 50),
+        children: <Widget>[
+          Image.asset(
+            'images/logo.png',
+            width: 100,
+            height: 100,
+          ),
+          TextField(decoration: InputDecoration(labelText: "E-mail")),
+          TextField(decoration: InputDecoration(labelText: "Senha")),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: RaisedButton(
                 child: Text("Entrar"),
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => TelaInicial()));
                 }),
-            RaisedButton(child: Text("Cadastrar"), onPressed: null)
-          ],
-        ),
+          ),
+          RaisedButton(child: Text("Cadastrar"), onPressed: () {})
+        ],
       ),
     );
   }
