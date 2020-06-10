54a4d413a070e055142ea8ad8e9fd77e39c610ad
Warley Soares <wss@cin.ufpe.br>
2020-06-10 10:27:33 -0300

    Mudanca de nome da classe principal e implementacao de rotas nomeadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:minhas_compras/views/login.dart';
 import 'package:minhas_compras/views/telaApresentacao.dart';
+import 'package:minhas_compras/views/telaInicial.dart';
 
 //Arquivo principal que usa o RunApp para chamar a classe principal e executar o app
 
 void main() {
-  runApp(PrimeiraTela());
+  runApp(MyApp());
 }
 
 /* Classe Principal que inicia o app. Ela deve utilizar o 'MaterialApp' como widget raiz da arvore, 
 e assim chamar na home dele outra classe. Nesse caso é a TelaApresentacao, que é a tela inicial do app.
 Resolvi tirar a página principal do main para organizar melhor em outra tela.*/
-class PrimeiraTela extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: TelaApresentacao(),
-        theme: ThemeData(
-          primaryColor: Color(
-              0xFF14213D), //Cor primária do app que vai definir a cor de elementos principais, como a appbar, e etc
-          accentColor: Color(
-              0xFFFCA311), //Cor definida para os elementos de destaque no app, como botões e etc
-          fontFamily: 'Montserrat',
-          appBarTheme: AppBarTheme(
-              textTheme: ThemeData.light().textTheme.copyWith(
-                  headline6: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700))),
-        ));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(
+            0xFF14213D), //Cor primária do app que vai definir a cor de elementos principais, como a appbar, e etc
+        accentColor: Color(
+            0xFFFCA311), //Cor definida para os elementos de destaque no app, como botões e etc
+        fontFamily: 'Montserrat',
+        appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                headline6: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700))),
+      ),
+      home: TelaApresentacao(),
+      routes: {
+        //Usando rotas nomeadas, assim nas paginas que precisam dessas rotas é só chamar o navigator com pushedname
+        'login': (context) => Login(),
+        'initialscreen': (context) => TelaInicial()
+      },
+    );
   }
 }
