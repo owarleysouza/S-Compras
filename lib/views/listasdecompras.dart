06511bd3e6c972d05f07e5b48b9cecb740124ae6
Warley Soares <wss@cin.ufpe.br>
2020-06-15 19:31:26 -0300

    refatoracao da tela para mostrar uma lista de listadecompra chamando o template de uma

--- lib/views/listasdecompras.dart
+++ lib/views/listasdecompras.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:minhas_compras/models/listadecompra.dart';
 import 'package:minhas_compras/models/produto.dart';
+import 'package:minhas_compras/views/listasdeComprasTemplate.dart';
 
 class ListasdeCompras extends StatefulWidget {
   @override
@@ -8,21 +9,40 @@ class ListasdeCompras extends StatefulWidget {
 }
 
 class _ListasdeComprasState extends State<ListasdeCompras> {
-  final _compras = ListadeCompra(
-      nome: "Compra de Julho",
-      data: DateTime.now(),
-      listadecompra: [
-        Produto(nome: "Açúcar", quantidade: "2x", categoria: "Grosso"),
-        Produto(nome: "Sal", quantidade: "Padrão", categoria: "Grosso"),
-        Produto(nome: "Desinfetante", quantidade: "3x", categoria: "LeH"),
-        Produto(nome: "Desodorante", quantidade: "1x", categoria: "LeH"),
-        Produto(nome: "Sabonete", quantidade: "Padrão", categoria: "LeH"),
-        Produto(nome: "Fio Dental", quantidade: "2x", categoria: "LeH"),
-        Produto(
-            nome: "Lâmina de Barbear", quantidade: "Padrão", categoria: "LeH"),
-        Produto(nome: "Peixe", quantidade: "3x", categoria: "Frios"),
-        Produto(nome: "Salsicha", quantidade: "1x", categoria: "Frios"),
-      ]);
+  final List<ListadeCompra> listadelistasdecompras = [
+    ListadeCompra(
+        nome: "Compra de Julho",
+        data: DateTime.now(),
+        listadeprodutos: [
+          Produto(nome: "Açúcar", quantidade: "2x", categoria: "Grosso"),
+          Produto(nome: "Sal", quantidade: "Padrão", categoria: "Grosso"),
+          Produto(nome: "Desinfetante", quantidade: "3x", categoria: "LeH"),
+          Produto(nome: "Desodorante", quantidade: "1x", categoria: "LeH"),
+          Produto(nome: "Sabonete", quantidade: "Padrão", categoria: "LeH"),
+          Produto(nome: "Fio Dental", quantidade: "2x", categoria: "LeH"),
+          Produto(
+              nome: "Lâmina de Barbear",
+              quantidade: "Padrão",
+              categoria: "LeH"),
+          Produto(nome: "Peixe", quantidade: "3x", categoria: "Frios"),
+          Produto(nome: "Salsicha", quantidade: "1x", categoria: "Frios"),
+        ]),
+    ListadeCompra(
+        nome: "Compra de Agosto",
+        data: DateTime.now(),
+        listadeprodutos: [
+          Produto(nome: "Açúcar", quantidade: "2x", categoria: "Grosso"),
+          Produto(nome: "Sal", quantidade: "Padrão", categoria: "Grosso"),
+          Produto(nome: "Sabonete", quantidade: "Padrão", categoria: "LeH"),
+          Produto(nome: "Fio Dental", quantidade: "2x", categoria: "LeH"),
+          Produto(
+              nome: "Lâmina de Barbear",
+              quantidade: "Padrão",
+              categoria: "LeH"),
+          Produto(nome: "Peixe", quantidade: "3x", categoria: "Frios"),
+          Produto(nome: "Salsicha", quantidade: "1x", categoria: "Frios"),
+        ])
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,15 +52,10 @@ class _ListasdeComprasState extends State<ListasdeCompras> {
       ),
       body: ListView(
         children: <Widget>[
-          Card(
-            child: Column(
-              children: <Widget>[
-                Text(_compras.nome),
-                Text(_compras.data.toString()),
-                Text(_compras.listadecompra.length.toString())
-              ],
-            ),
-          )
+          ...listadelistasdecompras.map((listadecompra) => ListaComprasTemplate(
+              nome: listadecompra.nome,
+              data: listadecompra.data,
+              quantidadeitens: listadecompra.listadeprodutos.length))
         ],
       ),
     );
