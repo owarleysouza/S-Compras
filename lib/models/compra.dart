ec41262d646d67dbae4a23f91cc00ea1cd6a7117
Warley Soares <wss@cin.ufpe.br>
2020-06-17 11:38:19 -0300

    add obrigatoriedade do nome da compra, e erro ainda a corrigir para lista de produtos constante

--- lib/models/compra.dart
+++ lib/models/compra.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:minhas_compras/models/produto.dart';
 
 class Compra {
@@ -5,5 +6,9 @@ class Compra {
   DateTime data;
   List<Produto> listadeprodutos;
 
-  Compra({this.nome, this.data, this.listadeprodutos});
+  Compra(
+      {@required this.nome,
+      this.data,
+      this.listadeprodutos =
+          const []}); //Tá dando erro porque não eh possivel add um novo produto a uma lista que tem essa lista de produtos vazia e constante. Resolver isso
 }
