83ba7d7cc0a25c0e8005351da4516ed6870e2ef5
Warley Soares <wss@cin.ufpe.br>
2020-07-03 11:33:28 -0300

    Implementacao de carrossel na tela de apresentacao e mudanca de ui na tela

--- lib/views/telaApresentacao.dart
+++ lib/views/telaApresentacao.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 //Tela de Apresentacao onde serão mostradas as funcionalidades do app
 class TelaApresentacao extends StatefulWidget {
@@ -11,56 +12,58 @@ class _TelaApresentacaoState extends State<TelaApresentacao> {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      color: Colors.white,
+      color: Theme.of(context).primaryColor,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Image.asset(
-            'assets/images/logo.png',
-            width: 80,
-            height: 80,
-          ),
-          Text(
-            "Carrossel",
-            style: TextStyle(decoration: TextDecoration.none, fontSize: 20),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  height: 180,
-                  color: Colors.black,
-                  child: Text(
-                    "Elemento 1",
-                    style: TextStyle(
-                        decoration: TextDecoration.none, fontSize: 15),
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                    height: 180,
-                    color: Colors.grey,
-                    child: Text(
-                      "Elemento 2",
-                      style: TextStyle(
-                          decoration: TextDecoration.none, fontSize: 15),
-                    )),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                    height: 180,
-                    color: Colors.blueGrey,
-                    child: Text(
-                      "Elemento 3",
-                      style: TextStyle(
-                          decoration: TextDecoration.none, fontSize: 15),
-                    )),
-              )
-            ],
+          CarouselSlider(
+            options: CarouselOptions(height: 400.0),
+            items: [
+              [
+                "assets/images/logo.png",
+                "Faça suas compras de forma simples e prática",
+              ],
+              [
+                "assets/images/add.png",
+                "Crie uma compra e adicione produtos nela",
+              ],
+              [
+                "assets/images/Frios.png",
+                "Divida os produtos das suas compras em categorias:  'Grosso', 'Limpeza e Higiene (LEH)' e 'Frios'",
+              ]
+            ].map((i) {
+              String img = i[0];
+              String text = i[1];
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                          color: Color(0xF264653),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Image.asset(
+                            img,
+                            width: 100,
+                            height: 100,
+                          ),
+                          Text(
+                            text,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white,
+                                decoration: TextDecoration.none),
+                          ),
+                        ],
+                      ));
+                },
+              );
+            }).toList(),
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 10),
