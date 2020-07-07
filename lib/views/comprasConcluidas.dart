22b2b6f2f33a1228cbc1f0a1af85e0549b4a519b
Warley Soares <wss@cin.ufpe.br>
2020-07-07 11:02:15 -0300

    Implementado compras concluidas

---
+++ lib/views/comprasConcluidas.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:minhas_compras/models/compra.dart';
+import 'package:minhas_compras/views/compraTemplate.dart';
+
+class ComprasConcluidas extends StatefulWidget {
+  final List<Compra> compras;
+  final Function delCompra;
+
+  ComprasConcluidas({@required this.compras, @required this.delCompra});
+
+  @override
+  _ComprasConcluidasState createState() => _ComprasConcluidasState();
+}
+
+class _ComprasConcluidasState extends State<ComprasConcluidas> {
+  _completeCompra(String id, bool iscomplete) {
+    for (Compra compra in widget.compras) {
+      if (compra.id == id) {
+        setState(() {
+          compra.iscompleted = iscomplete;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Compras Realizadas"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ...widget.compras.map((compra) {
+            if (compra.iscompleted == true) {
+              return CompraTemplate(
+                compra: compra,
+                delCompra: widget.delCompra,
+                completeCompra: _completeCompra,
+              );
+            } else {
+              return Container();
+            }
+          }),
+          const SizedBox(
+            height: 70,
+          )
+        ],
+      ),
+    );
+  }
+}
