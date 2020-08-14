e2454660129037b9084aa12a9f0920f6208c1287
Warley Soares <wss@cin.ufpe.br>
2020-08-14 11:52:29 -0300

    Mudanca na interface de uma compra na tela, removendo a quantidade de produtos nela e add botao para editar uma compra

--- lib/widgets/shop_item.dart
+++ lib/widgets/shop_item.dart
@@ -54,47 +54,57 @@ class ShopItem extends StatelessWidget {
               );
             }),
         child: Card(
-          color: compra.iscompleted == true ? Colors.green[200] : Colors.white,
+          color: compra.iscompleted == true
+              ? Colors.lightGreen[300]
+              : Colors.white,
           elevation: 5,
-          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 10),
             child: ListTile(
-              leading: Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: CircleAvatar(
-                  backgroundColor: const Color(0xFF14213D),
-                  radius: 30,
-                  child: Text(compra.listadeprodutos.length.toString()),
-                ),
+              leading: Icon(
+                Icons.shopping_cart,
+                color: Theme.of(context).primaryColor,
+                size: 40,
               ),
               title: Text(compra.nome),
               subtitle: Text((DateFormat('dd/MM/y').format(compra.data))),
-              trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  color: Theme.of(context).errorColor,
-                  onPressed: () => showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text("Apagar Compra"),
-                          content: Text(
-                              "Tem certeza que deseja APAGAR essa compra?"),
-                          actions: <Widget>[
-                            FlatButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: Text("Cancelar")),
-                            FlatButton(
-                                onPressed: () {
-                                  delCompra(compra.id);
-                                  Navigator.pop(context);
-                                },
-                                child: Text("OK"))
-                          ],
-                        );
-                      })),
+              trailing: Container(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                        icon: Icon(Icons.edit,
+                            color: Theme.of(context).accentColor),
+                        onPressed: null),
+                    IconButton(
+                        icon: const Icon(Icons.delete),
+                        color: Theme.of(context).errorColor,
+                        onPressed: () => showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Apagar Compra"),
+                                content: Text(
+                                    "Tem certeza que deseja APAGAR essa compra?"),
+                                actions: <Widget>[
+                                  FlatButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("Cancelar")),
+                                  FlatButton(
+                                      onPressed: () {
+                                        delCompra(compra.id);
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("OK"))
+                                ],
+                              );
+                            })),
+                  ],
+                ),
+              ),
             ),
           ),
         ));
