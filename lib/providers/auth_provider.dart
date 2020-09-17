c0817424c9c3996da3da944077ab1e41c8d2f035
Warley Soares <wss@cin.ufpe.br>
2020-09-17 10:40:26 -0300

    Refatorado metodo de autenticacao no provider para login e cadastro

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,18 +2,31 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:minhas_compras/exceptions/auth_exception.dart';
 
 class AuthProvider with ChangeNotifier {
-  static const _url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDN_eMHVzkyQAb3w_HgejZo3ZsVIxb4YPg';
+  Future<void> authenticate(
+      String email, String password, String urlSegment) async {
+    final _url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDN_eMHVzkyQAb3w_HgejZo3ZsVIxb4YPg';
 
-  Future<void> signup(String email, String password) async {
     final response = await http.post(_url,
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
 
-    print(json.decode(response.body));
+    final responseBody = json.decode(response.body);
+    if (responseBody["error"] != null) {
+      throw AuthException(responseBody["error"]["message"]);
+    }
 
     return Future.value();
+  }
+
+  Future<void> login(String email, String password) async {
+    return authenticate(email, password, "signInWithPassword");
+  }
+
+  Future<void> signup(String email, String password) async {
+    return authenticate(email, password, "signUp");
   }
 }
