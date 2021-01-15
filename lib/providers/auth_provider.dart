daf8bad622fe3a5d0531873ebcd93cf20dc76315
Warley Soares <wss@cin.ufpe.br>
2021-01-15 15:13:25 -0300

    arquivos de acesso gerados e adicionados no git ignore

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:minhas_compras/data/store.dart';
 import 'package:minhas_compras/exceptions/auth_exception.dart';
+import 'package:minhas_compras/utils/access_key.dart';
 
 class AuthProvider with ChangeNotifier {
   String _userId;
@@ -46,8 +47,9 @@ class AuthProvider with ChangeNotifier {
 
   Future<void> authenticate(
       String email, String password, String urlSegment) async {
-    final _url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDN_eMHVzkyQAb3w_HgejZo3ZsVIxb4YPg';
+    final _url = urlSegment == "signInWithPassword"
+        ? ConstantsKey.BASE_API_KEY_URL_SIGNIN
+        : ConstantsKey.BASE_API_KEY_URL_SIGNUP;
 
     final response = await http.post(_url,
         body: json.encode(
